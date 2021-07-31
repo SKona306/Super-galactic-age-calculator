0b96d3353baf31dccf0812d8d929c84dc8ebe66f
Shaun Kent <shaunkent81@gmail.com>
2021-07-31 12:29:27 -0700

    add code to pass first test for venus.js file

--- __tests__/venus.test.js
+++ __tests__/venus.test.js
@@ -4,7 +4,7 @@ describe('venusAge', () => {
 
   test('shoul correctly take in users age(earth years) and return users age in venus years.', () => {
     let venus = new venusAge();
-    venusAge.calculateVenusAge(24);
+    venus.calculateVenusAge(24);
     expect(venus.ageOnVenus).toEqual(39);
   });
 });
