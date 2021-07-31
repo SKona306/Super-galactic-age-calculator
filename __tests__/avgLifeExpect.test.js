c90cd2b0b566f768615b69817a7f2c7c79cfab8c
Shaun Kent <shaunkent81@gmail.com>
2021-07-31 14:38:38 -0700

    add code to pass test for avgLifeExpect.js file

--- __tests__/avgLifeExpect.test.js
+++ __tests__/avgLifeExpect.test.js
@@ -1,9 +1,10 @@
+import { AvgLifeExpect } from "../src/avgLifeExpect";
 
 describe('avgLifeExpect', () => {
 
   test('should correctly calculate users average life expectancy based on results of questionare.', () => {
-    let lifeExpectancy = new AvgLifeExpect (male, active, true, true, hs, 6);
-    lifeExpectancy.calculateAvgLifeExpect();
-    expect(lifeExpectancy.lifeExpectancy).toEqual(72);
+    let lifeExpectancy = new AvgLifeExpect("male", "active", true, true, "hs", 6);
+    lifeExpectancy.calculateAvgLifeExpectancy();
+    expect(lifeExpectancy.avgLifeExpect).toEqual(72);
   });
 });
