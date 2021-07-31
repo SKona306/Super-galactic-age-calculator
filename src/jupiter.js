b639a1178507ba3b44247e16111260827050dd0c
Shaun Kent <shaunkent81@gmail.com>
2021-07-31 13:19:52 -0700

    add code to calculate age on jupiter

---
+++ src/jupiter.js
@@ -0,0 +1,14 @@
+
+export class JupiterAge {
+  constructor(earthAge) {
+    this.earthAge = earthAge;
+    this.jupiterAgeConstant = 11.86;
+    this.ageOnJupiter = 0;
+  }
+  calculateJupiterAge = () => {
+    this.ageOnJupiter = Math.round(this.earthAge / this.jupiterAgeConstant);
+    if(this.ageOnJupiter < 1) {
+      this.ageOnJupiter = 1;
+    }
+  }
+}
