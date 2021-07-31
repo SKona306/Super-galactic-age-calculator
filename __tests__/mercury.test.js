5d7c209a8480436eec669eba7386742452b16696
Shaun Kent <shaunkent81@gmail.com>
2021-07-31 12:36:11 -0700

    add first test for mercury.js file

---
+++ __tests__/mercury.test.js
@@ -0,0 +1,9 @@
+
+describe('mercuryAge', () => {
+  
+  test('should correctly take in user age(earth years) and return age in mercury years.', () => {
+    let mercury = new MercuryAge();
+    mercury.calculateMercuryAge(24);
+    expect(mercury.ageOnMercury).toEqual(100);
+  });
+});
