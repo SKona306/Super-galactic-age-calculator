8942cbe39ebfc986534b964721953edd62d3cca0
Shaun Kent <shaunkent81@gmail.com>
2021-07-31 12:26:19 -0700

    add good fail in venus.js test

--- __tests__/venus.test.js
+++ __tests__/venus.test.js
@@ -1,10 +1,10 @@
-
+import { venusAge } from "../src/venus";
 
 describe('venusAge', () => {
 
   test('shoul correctly take in users age(earth years) and return users age in venus years.', () => {
     let venus = new venusAge();
     venusAge.calculateVenusAge(24);
-    expect(venus.venusAge).toEqual(39);
+    expect(venus.ageOnVenus).toEqual(39);
   });
 });
