9bd2a5e85c6e21815d0d7b3d9dae22c19e20d383
Shaun Kent <shaunkent81@gmail.com>
2021-07-31 13:20:23 -0700

    add code to calculate users age on mars

---
+++ src/mars.js
@@ -0,0 +1,11 @@
+
+export class MarsAge {
+  constructor(earthAge) {
+    this.earthAge = earthAge;
+    this.marsAgeConstant = 1.88;
+    this.ageOnMars = 0;
+  }
+  calculateMarsAge = () => {
+    this.ageOnMars = Math.round(this.earthAge / this.marsAgeConstant);
+  }
+}
