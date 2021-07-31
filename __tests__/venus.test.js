5b7f12cde31d31b0122e28b447b2233b61b9849f
Shaun Kent <shaunkent81@gmail.com>
2021-07-31 13:12:43 -0700

    change the format for planet age calculation

--- __tests__/venus.test.js
+++ __tests__/venus.test.js
@@ -1,9 +1,9 @@
-import { venusAge } from "../src/venus";
+import { VenusAge } from "../src/venus";
 
 describe('venusAge', () => {
 
   test('shoul correctly take in users age(earth years) and return users age in venus years.', () => {
-    let venus = new venusAge();
+    let venus = new VenusAge();
     venus.calculateVenusAge(24);
     expect(venus.ageOnVenus).toEqual(39);
   });
