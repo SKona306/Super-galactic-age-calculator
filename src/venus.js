57df3322412d10056204e3ff99596b0808eaf470
Shaun Kent <shaunkent81@gmail.com>
2021-07-31 19:12:01 -0700

    add method to calculate difference in life expectancy and venus age

--- src/venus.js
+++ src/venus.js
@@ -3,11 +3,12 @@ export class VenusAge {
     this.earthAge = earthAge;
     this.venusAgeConstant = 0.62;
     this.ageOnVenus = 0;
+    this.diffInExpectancyAndAge = 0;
   }
   calculateVenusAge () {
     this.ageOnVenus = Math.round(this.earthAge / this.venusAgeConstant);
   } 
-  aboveOrBelowLifeExpect () {
-
+  aboveOrBelowLifeExpectancy(lifeExpectancy) {
+    this.diffInExpectancyAndAge = lifeExpectancy.avgLifeExpect - this.ageOnVenus;
   }
 }
