7c2e009f38183ca6f94993d4a4256762cb93a010
Shaun Kent <shaunkent81@gmail.com>
2021-07-31 18:32:19 -0700

    correct third test and add code to pass

--- __tests__/mars.test.js
+++ __tests__/mars.test.js
@@ -9,9 +9,11 @@ describe('marsAge', () => {
     expect(mars.ageOnMars).toEqual(13);
   });
 
-  test('should correctly find the difference between mars age and life expectancy and return how many years under the user is' () => {
+  test('should correctly find the difference between mars age and life expectancy and return how many years under the user is', () => {
     let mars = new MarsAge(24);
+    mars.calculateMarsAge();
     let lifeExpectancy = new AvgLifeExpect("male", "active", true, true, "hs", 6);
+    lifeExpectancy.calculateAvgLifeExpectancy();
     mars.aboveOrBelowLifeExpectancy(lifeExpectancy);
     expect(mars.diffInExpectancyAndAge).toEqual(59);
   });
