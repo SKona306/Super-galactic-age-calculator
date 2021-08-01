7112195026cb9d297a43bdd8dcd26cd781dd0524
Shaun Kent <shaunkent81@gmail.com>
2021-07-31 19:08:00 -0700

    add second test for venus.js file

--- __tests__/venus.test.js
+++ __tests__/venus.test.js
@@ -1,10 +1,19 @@
 import { VenusAge } from "../src/venus";
+import { AvgLifeExpect } from "../src/avgLifeExpect";
 
 describe('venusAge', () => {
 
   test('shoul correctly take in users age(earth years) and return users age in venus years.', () => {
-    let venus = new VenusAge();
-    venus.calculateVenusAge(24);
+    let venus = new VenusAge(24);
+    venus.calculateVenusAge();
     expect(venus.ageOnVenus).toEqual(39);
+  });
+  test('should correctly find the difference between users venus age and life expectancy and return how far below', () => {
+    let venus = new VenusAge(24);
+    venus.calculateVenusAge();
+    let lifeExpectancy = new AvgLifeExpect("male", "active", true, true, "hs", 6);
+    lifeExpectancy.calculateAvgLifeExpectancy();
+    venus.aboveOrBelowLifeExpectancy(lifeExpectancy);
+    expect(venus.diffInExpectancyAndAge).toEqual(33);
   });
 });
