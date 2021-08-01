930a17beeb56a5821bfebee954a975ea90d6e257
Shaun Kent <shaunkent81@gmail.com>
2021-07-31 18:41:52 -0700

    add second test for mercury.js file

--- __tests__/mercury.test.js
+++ __tests__/mercury.test.js
@@ -1,10 +1,20 @@
+import { AvgLifeExpect } from "../src/avgLifeExpect";
 import { MercuryAge } from "../src/mercury";
 
 describe('mercuryAge', () => {
   
   test('should correctly take in user age(earth years) and return age in mercury years.', () => {
-    let mercury = new MercuryAge();
-    mercury.calculateMercuryAge(24);
+    let mercury = new MercuryAge(24);
+    mercury.calculateMercuryAge();
     expect(mercury.ageOnMercury).toEqual(100);
+  });
+
+  test('should correctly find the difference between users mercury age and life expectancy and return how far below', () => {
+    let mercury = new MercuryAge(15);
+    mercury.calculateMercuryAge();
+    let avgLifeExpect = new AvgLifeExpect("male", "active", true, true, "hs", 6);
+    avgLifeExpect.calculateAvgLifeExpectancy();
+    mercury.aboveOrBelowLifeExpect(lifeExpect);
+    expect(mercury.diffInExpectancyAndAge).toEqual(9);
   });
 });
