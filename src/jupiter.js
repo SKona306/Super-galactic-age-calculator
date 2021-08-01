28c7febdaa6a2a87ceafc3096093499847e27178
Shaun Kent <shaunkent81@gmail.com>
2021-07-31 18:30:44 -0700

    remove uneccessary import link

--- src/jupiter.js
+++ src/jupiter.js
@@ -4,11 +4,15 @@ export class JupiterAge {
     this.earthAge = earthAge;
     this.jupiterAgeConstant = 11.86;
     this.ageOnJupiter = 0;
+    this.diffInExpectancyAndAge = 0;
   }
   calculateJupiterAge () {
     this.ageOnJupiter = Math.round(this.earthAge / this.jupiterAgeConstant);
     if(this.ageOnJupiter < 1) {
       this.ageOnJupiter = 1;
     }
+  }
+  aboveOrBelowLifeExpectancy (lifeExpectancy) {
+    this.diffInExpectancyAndAge = lifeExpectancy.avgLifeExpect - this.ageOnJupiter;
   }
 }
